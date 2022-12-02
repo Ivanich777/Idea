b224c5db14ab7897a370282a670fb9b80495bc18
sharkeeyy <sfmei2003@yandex.ru>
2022-12-02 14:39:22 +0300

    nav-footer

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<><Header /><Footer /></>}>
+          <Route path="/main" element={<Header />} />
+
+        </Route>
+      </Routes>
     </div>
   );
 }

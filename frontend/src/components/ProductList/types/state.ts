74a6912b5a499dda8fd8c8157deaa616702e4240
Alexandra <lubnina.alexandra@gmail.com>
2022-12-02 16:53:43 +0300

    feat: added productListSlice

---
+++ frontend/src/components/ProductList/types/state.ts
@@ -0,0 +1,15 @@
+
+export interface Product {
+  article: number,
+  title: string,
+  description: string,
+  count: number,
+  price: number,
+  idCategory: number,
+}
+
+export interface State {
+  products: Product[];
+}
+
+

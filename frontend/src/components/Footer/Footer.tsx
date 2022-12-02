14af6c6bd7397a3cf52dce985f1f51253f9c9d25
sharkeeyy <sfmei2003@yandex.ru>
2022-12-02 20:14:01 +0300

    footer fix

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,15 +4,15 @@ import { Container, Grid, Box, Link } from '@mui/material';
 export default function Footer(): JSX.Element {
   return (
     <Box
-      px={{ xs: 3, sm: 10 }}
-      py={{ xs: 5, sm: 10 }}
+      px={{ xs: 2, sm: 2, lg: 2 }}
+      py={{ xs: 2, sm: 2, lg: 4 }}
       bgcolor="text.secondary"
       color="white"
     >
       <Container maxWidth="lg">
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
-            <Box>КОНТАКТЫ</Box>
+            <Box py={{ xs: 2, sm: 2, lg: 2 }}>КОНТАКТЫ</Box>
             <Box>
               <Link href="/" color="inherit">
                 Наша компания
@@ -35,7 +35,7 @@ export default function Footer(): JSX.Element {
             </Box>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Box>ИНТЕРНЕТ-МАГАЗИН</Box>
+            <Box py={{ xs: 2, sm: 2, lg: 2 }}>ИНТЕРНЕТ-МАГАЗИН</Box>
             <Box>
               <Link href="/" color="inherit">
                 Каталог
@@ -63,17 +63,29 @@ export default function Footer(): JSX.Element {
             </Box>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
-              <Box sx={{ display: { xs: 'none', lg: 'block' } }}>
-                <span>Мы принимаем:</span>
-              </Box>
+            <Box>
+              {/* <Mapp /> */}
+            </Box>
+            <Box pt={{ xs: 2, sm: 2, lg: 2 }}>
+              <span>Лахта-центр, адрес: Высотная улица, 1, Санкт-Петербург, 197229</span>
+            </Box>
+            <Box>
+              <span>email: lahta-construction@gmail.com</span>
+            </Box>
+            <Box>
+              <span>phone: +7920-660-8423</span>
+            </Box>
+            <Box
+              sx={{ display: 'flex', justifyContent: 'space-around' }}
+              pt={{ xs: 2, sm: 3, lg: 3 }}
+            >
               <img width="49" height="14" alt="Мир" src="//res.cloudinary.com/lmru/image/upload/index/mir-logo.svg" />
               <img width="49" height="14" alt="Visa" src="//res.cloudinary.com/lmru/image/upload/index/visa-logo.svg" />
               <img width="28" height="19" alt="Mastercard" src="//res.cloudinary.com/lmru/image/upload/index/mastercard-logo.svg" />
             </Box>
           </Grid>
         </Grid>
-        <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
+        <Box textAlign="center" pt={{ xs: 2, sm: 2, lg: 2 }} pb={{ xs: 2, sm: 0, lg: 0 }}>
           IdeaProject &reg; {new Date().getFullYear()}
         </Box>
       </Container>

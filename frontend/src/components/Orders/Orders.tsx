f86b8fde2ce4dfffb408a50ab5b56b39ce941373
Alexandra <lubnina.alexandra@gmail.com>
2022-12-04 14:49:26 +0300

    feat: deleted btn and load orders with update

--- frontend/src/components/Orders/Orders.tsx
+++ frontend/src/components/Orders/Orders.tsx
@@ -4,21 +4,13 @@ import { RootState, useAppDispatch } from '../../store';
 import { addAsyncOrders } from './orderSlice';
 
 function Orders(): JSX.Element {
-  const { orders } = useSelector((state: RootState) => state.orders);
-  console.log(orders);
-
   const dispatch = useAppDispatch();
-
-  function wasd(): void {
-    dispatch(addAsyncOrders());
-  }
+  dispatch(addAsyncOrders());
+  const { orders } = useSelector((state: RootState) => state.orders);
 
   return (
     <div>
       <div>Orders</div>
-      <button onClick={() => wasd()} type="button">
-        add async orders
-      </button>
       <ul>
         {orders.map((order) => (
           <li key={order.id}>{order.status}</li>

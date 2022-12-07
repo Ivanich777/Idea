352e56e7f1775af8621e2d118e655380755ec944
Alexandra <lubnina.alexandra@gmail.com>
2022-12-07 16:08:28 +0300

    feat: added buttons

--- frontend/src/components/Basket/BasketItem/BasketItem.tsx
+++ frontend/src/components/Basket/BasketItem/BasketItem.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
-import { Box, IconButton, ListItem, ListItemText, Stack, Typography } from '@mui/material'
+import { Box, Button, IconButton, ListItem, ListItemText, Stack, Typography } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete';
 import { BasketItem } from './State/state';
 import { RootState, useAppDispatch } from '../../../store';
 import { actualOrder, deleteBasketItem } from '../../ProductCard/basketSlice';
 import { useSelector } from 'react-redux';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 function BasketElement({ item }: BasketItem | any): JSX.Element {
   const { user } = useSelector((state: RootState) => state.users);
@@ -34,28 +36,44 @@ function BasketElement({ item }: BasketItem | any): JSX.Element {
         secondaryAction={
           <IconButton edge="end" aria-label="delete"
           >
-            <DeleteIcon onClick={handleDeleteBasketItem} 
-            style={{paddingRight:'30px'}}/>
+            <DeleteIcon onClick={handleDeleteBasketItem}
+              style={{ paddingRight: '30px' }} />
           </IconButton>
         }
-        style={{borderBottom:'2px solid #FFE4C4', borderRadius:'10px'}}
+        style={{ borderBottom: '2px solid #FFE4C4', borderRadius: '10px' }}
       >
-        <img src={`${item.Images[0].path}`} style={{ width: '100px',height:'100px' ,borderRadius:'10px'}}></img>
-        <div className='component' style={{display:'flex',flexDirection:'column', paddingLeft:'60px'}}>
-        <ListItemText
-          primary={`${item?.title}`}
-        />
-        <ListItemText primary={`Артикул: ${item?.article}`}
-        style={{fontFamily:'Georgia, serif', letterSpacing:'2.0px'}}
-        />
-         </div>
-         <div className='component_two' style={{display:'flex',flexDirection:'column',paddingLeft:'300px'}}>
-        <ListItemText
-          primary={`${item?.price} ₽`}
-        />
-        <ListItemText primary={`кол-во:${item?.orderCount}`} />
-        <ListItemText primary={`Итого: ${item?.orderCount * item?.price}`} />
-        </div>     
+        <img src={`${item.Images[0].path}`} style={{ width: '100px', height: '100px', borderRadius: '10px' }}></img>
+        <div className='component' style={{ display: 'flex', flexDirection: 'column', paddingLeft: '60px' }}>
+          <ListItemText
+            primary={`${item?.title}`}
+          />
+          <ListItemText primary={`Артикул: ${item?.article}`}
+            style={{ fontFamily: 'Georgia, serif', letterSpacing: '2.0px' }}
+          />
+        </div>
+        <div className='component_two' style={{ display: 'flex', flexDirection: 'column', paddingLeft: '300px' }}>
+          <ListItemText
+            primary={`${item?.price} ₽`}
+          />
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            {/* <ListItemText > */}
+              <Button
+                aria-label="reduce"
+              >
+                <RemoveIcon fontSize="small" />
+              </Button>
+              <ListItemText>
+                {`кол-во: ${item?.orderCount}`}
+              </ListItemText>
+              <Button
+                aria-label="increase"
+              >
+                <AddIcon fontSize="small" />
+              </Button>
+            {/* </ListItemText> */}
+          </div>
+          <ListItemText primary={`Итого: ${item?.orderCount * item?.price}`} />
+        </div>
       </ListItem>
     </Box >
   )

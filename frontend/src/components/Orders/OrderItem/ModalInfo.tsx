21d8332ebf5093bae395e710144c0a268100ab25
dmt-denisov <double.dmt>
2022-12-07 17:15:58 +0300

    fix

--- frontend/src/components/Orders/OrderItem/ModalInfo.tsx
+++ frontend/src/components/Orders/OrderItem/ModalInfo.tsx
@@ -105,7 +105,7 @@ function ModalInfo({ order, orderItems, showModal, handleManualClose }: IModel):
                 <TableCell />
                 <TableCell />
                 <TableCell />
-                <TableCell align="right">{`${sum()} ₽`}</TableCell>
+                <TableCell align="right">{`Итого: ${sum()} ₽`}</TableCell>
               </TableFooter>
             </Table>
           </TableContainer>
